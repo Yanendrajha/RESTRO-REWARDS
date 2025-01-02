0cf3ef104fee1f3018245c9f07f4d4d3f2197808
Yanendrajha <yanendrajha37@gmail.com>
2025-01-02 13:58:42 +0530

    Spring Boot Auto-Config & OverRiding Config

--- 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -1,8 +1,18 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -35,6 +45,10 @@ import org.springframework.boot.SpringApplication;
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
 
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@ConfigurationPropertiesScan
+@EnableConfigurationProperties(RewardsRecipientProperties.class)
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
@@ -44,6 +58,18 @@ public class RewardsApplication {
     public static void main(String[] args) {
         SpringApplication.run(RewardsApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        Long numberOfAccounts = jdbcTemplate.queryForObject(SQL, Long.class);
+        return args -> logger.info("hello there are {} accounts", numberOfAccounts);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner2(RewardsRecipientProperties rewardsRecipientProperties) {
+        return args -> System.out.println("Recipient: " + rewardsRecipientProperties.getName());
+    }
+
 
     // TODO-04 : Let Spring Boot execute database scripts
     // - Move the SQL scripts (schema.sql and data.sql)
